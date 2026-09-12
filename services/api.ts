f231b02475526522d69c215c90bf50fab97b7dbc
Sandeep Singh <jaspalshubh12@gmail.com>
2026-09-12 22:42:42 +0530

    Configure environment based API URL

--- services/api.ts
+++ services/api.ts
@@ -1,10 +1,15 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
+const API_BASE_URL =
+  process.env.EXPO_PUBLIC_API_URL ||
+  "http://192.168.31.237:5000/api";
+
 const API = axios.create({
- baseURL: "http://192.168.31.237:5000/api",
+  baseURL: API_BASE_URL,
 });
 
+console.log("🔥 DRIVER API:", API.defaults.baseURL);
 
 API.interceptors.request.use(async (config) => {
   const token = await AsyncStorage.getItem("token");
